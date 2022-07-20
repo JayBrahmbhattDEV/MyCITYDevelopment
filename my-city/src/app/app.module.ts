ff27e55d65acf24ffba86b4d310f69447ef17015
JayBrahmbhattDEV <jaybrahmbhatt094@gmail.com>
2022-07-21 00:34:10 +0530

    Created a simple login page
    Missing validation and Native Storage

--- my-city/src/app/app.module.ts
+++ my-city/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -9,8 +11,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, NativeStorage],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- my-city/src/app/register/register.page.ts
+++ my-city/src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  isSubmitted = false;
+  constructor(public formBuilder: FormBuilder, private nativeStorage: NativeStorage) { }
 
   ngOnInit() {
   }
 
+  LoginForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    contact: ['', Validators.required]
+  })
+
+  get errorControl() {
+    return this.LoginForm.controls;
+  }
+
+  submit(){
+    console.log(this.LoginForm.value);
+    this.nativeStorage.setItem('name', this.LoginForm.controls.name.value)
+  .then(
+    () => console.log('Stored item!'),
+    error => console.error('Error storing item', error)
+  );
+  }
+  
 }
