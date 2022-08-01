20c114dc016e8dc7913de6b1c6f7ba9cf8756078
JayBrahmbhattDEV <jaybrahmbhatt094@gmail.com>
2022-08-01 23:10:20 +0530

    Added list and login tweaks

--- my-city/src/app/newreport/newreport.page.ts
+++ my-city/src/app/newreport/newreport.page.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-newreport',
   templateUrl: './newreport.page.html',
   styleUrls: ['./newreport.page.scss'],
 })
 export class NewreportPage implements OnInit {
-  radioForm: FormGroup;
+  newReport: FormGroup;
   constructor(
     public formBuilder: FormBuilder,
     private router: Router) { }
 
   ngOnInit() {
-    this.radioForm = this.formBuilder.group({
-      
-    })
+    this.newReport = this.formBuilder.group({
+        lrash: ['']
+    });
+  }
+
+  test(){
+    console.log("dwef");
+    
   }
 
 }
