ad5bb3694470c84783bdd15bbf5c15e839a8c745
Hiren Shekhda <hirenshekhda@frami.eco>
2022-08-03 20:58:55 +0530

    Removed menu-controller

--- my-city/src/app/login/login.page.ts
+++ my-city/src/app/login/login.page.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { MenuController, NavController } from '@ionic/angular';
 import { AccountService } from '../services/account.service';
 import { Storage } from '@ionic/storage-angular';
+import { CommonService } from '../services/common.service';
 
 @Component({
   selector: 'app-login',
@@ -20,7 +21,7 @@ export class LoginPage implements OnInit {
     public storage: Storage,
     public navController: NavController,
     private accountService: AccountService,
-    private menuController: MenuController
+    public commonService: CommonService
   ) {}
 
   ngOnInit() {
@@ -29,20 +30,22 @@ export class LoginPage implements OnInit {
     });
   }
 
-  ionViewWillEnter() {
-    this.menuController.enable(false);
-  }
-
   get errorCtr() {
     return this.LoginForm.controls;
   }
 
   submitForm() {
-    if (this.LoginForm.controls.loginVal.value !== '') {
+    if (this.LoginForm.valid) {
       this.submitted = true;
+      this.commonService.presentLoading();
       this.accountService.login(this.LoginForm.value).subscribe(
-        (response) => {
-          this.router.navigateByUrl(`/dashboard`);
+        (response: any) => {
+          this.commonService.hideLoading();
+          if (response.success) {
+            const { token, user } = response.data;
+            this.accountService.userDetails = user;
+            this.router.navigateByUrl(`/dashboard`);
+          }
         },
         (e) => {
           console.log(e);
