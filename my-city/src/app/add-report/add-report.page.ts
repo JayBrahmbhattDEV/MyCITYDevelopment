70383633b1b8a64fa63afb0ce82482057d3da190
JayBrahmbhattDEV <jaybrahmbhatt094@gmail.com>
2022-08-14 18:40:08 +0530

    Bringing add rep

--- my-city/src/app/add-report/add-report.page.ts
+++ my-city/src/app/add-report/add-report.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
 import { IonSelect } from '@ionic/angular';
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReportsService } from '../services/reports.service';
 
 @Component({
   selector: 'app-add-report',
@@ -124,28 +126,107 @@ export class AddReportPage implements OnInit {
   ];
 
   subCategoriesTemp = [];
-  isOther = false;
-  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  options: CameraOptions = {
+    quality: 30,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+  };
+
+  image =
+    'https://i0.wp.com/www.opindia.com/wp-content/uploads/2021/02/Ahmedabad-630x381-1.jpg?fit=630%2C381&ssl=1';
+  reportForm: FormGroup;
+  constructor(
+    private camera: Camera,
+    private formBuilder: FormBuilder,
+    private reportService: ReportsService
+  ) {}
+
+  ngOnInit() {
+    this.initReportForm();
+  }
 
   getSubCategory($event) {
     this.ddSubCategory.value = null;
-    this.isOther = false;
     this.subCategoriesTemp = this.subCategories.filter(
       (x) => x.parent === $event.detail.value
     );
   }
 
-  subCategoryChange($event) {
-    if ($event && $event?.detail?.value?.parent === 7) {
-      this.isOther = true;
-    } else {
-      this.isOther = false;
-    }
+  openCamera() {
+    this.camera
+      .getPicture(this.options)
+      .then((imageData) => {
+        this.image = 'data:image/jpeg;base64,' + imageData;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   }
 
-  addCord(){
-    this.router.navigateByUrl(`/createreport`);
+  initReportForm() {
+    this.reportForm = this.formBuilder.group({
+      address: ' Zydus Hospital , Thaltej , Ahemdabad - 360059',
+      description: '',
+      category: '',
+      subCategory: '',
+      // location: { latitude: 1111, longitude: 122121 },
+    });
+  }
+
+  private convertBase64ToBlob(base64: string) {
+    const info = this.getInfoFromBase64(base64);
+    const sliceSize = 512;
+    const byteCharacters = window.atob(info.rawBase64);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+      const byteNumbers = new Array(slice.length);
+
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      byteArrays.push(new Uint8Array(byteNumbers));
+    }
+
+    return new Blob(byteArrays, { type: info.mime });
+  }
+
+  private getInfoFromBase64(base64: string) {
+    const meta = base64.split(',')[0];
+    const rawBase64 = base64.split(',')[1].replace(/\s/g, '');
+    const mime = /:([^;]+);/.exec(meta)[1];
+    const extension = /\/([^;]+);/.exec(meta)[1];
+
+    return {
+      mime,
+      extension,
+      meta,
+      rawBase64,
+    };
+  }
+
+  addReport() {
+    const blob = this.convertBase64ToBlob(this.image);
+    const reportData = new FormData();
+    reportData.append('imgFile', blob, 'problem.jpg');
+    reportData.append('location',JSON.stringify({
+      latitude: '123',
+      longitude: '123',
+    }));
+    Object.keys(this.reportForm.value).forEach((key) => {
+      reportData.append(key, this.reportForm.value[key]);
+    });
+    this.reportService.addReport(reportData).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (e) => {
+        console.log(e);
+      }
+    );
   }
 }
