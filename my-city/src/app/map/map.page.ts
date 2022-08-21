f4b05c76b04216b8f9bb4edcd753e9b394fcd00e
Hiren Shekhda <hirenshekhda@frami.eco>
2022-08-21 16:56:23 +0530

    Map - map - Add map page and remove old map page

---
+++ my-city/src/app/map/map.page.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Icon, Map, Marker, TileLayer } from 'leaflet';
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.page.html',
+  styleUrls: ['./map.page.scss'],
+})
+export class MapPage implements OnInit {
+  map: Map;
+  latLong = [23.030357, 72.517845];
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private geolocation: Geolocation
+  ) {}
+
+  ngOnInit(): void {}
+
+  ionViewDidEnter() {
+    this.getCurrentLocation();
+    this.map = new Map('map').setView([0, 0], 15);
+    this.map.removeControl(this.map.zoomControl);
+    new TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap',
+    }).addTo(this.map);
+
+    const marker = new Marker(this.latLong, {
+      icon: new Icon({
+        iconUrl: './assets/images/marker-icon.png',
+      }),
+      draggable: true,
+    }).addTo(this.map);
+
+    marker.addEventListener('dragend', (e) => {
+      console.log(marker.getLatLng().lat);
+      console.log(marker.getLatLng().lng);
+    });
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
+  }
+
+  getCurrentLocation() {
+    this.geolocation
+      .getCurrentPosition()
+      .then((response) => {
+        this.latLong = [];
+        this.latLong.push(response.coords.latitude);
+        this.latLong.push(response.coords.longitude);
+        this.map.flyTo(
+          [response.coords.latitude, response.coords.longitude],
+          13
+        );
+
+        console.log(this.latLong);
+      })
+      .catch((e) => console.log(e));
+  }
+}
