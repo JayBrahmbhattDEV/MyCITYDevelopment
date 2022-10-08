34816c6adba07c277a0a508c8e39d1a406a8c1af
poojan3037 <poojan3037@gmail.com>
2022-10-08 15:21:16 +0530

    Category

--- server/app.js
+++ server/app.js
@@ -4,28 +4,28 @@ const cors = require("cors");
 require("dotenv").config();
 
 const userRouter = require("./routes/userRoute");
-const recordRouter = require("./routes/recordRoute")
+const recordRouter = require("./routes/recordRoute");
 const connectDB = require("./db/connect");
+const categoryRouter = require("./routes/categoryRoute");
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI;
 
 //middlewares
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/user", userRouter);
-app.use("/api/v1/record", recordRouter)
-
+app.use("/api/v1/record", recordRouter);
+app.use("/api/v1/category", categoryRouter);
 
 const start = async () => {
-    try {
-        await connectDB(MONGO_URI);
-        app.listen(PORT, () => console.log(`server is listing on port : ${PORT}`))
-    } catch (err) {
-        console.log(err);
-    }
-
-}
+  try {
+    await connectDB(MONGO_URI);
+    app.listen(PORT, () => console.log(`server is listing on port : ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 start();
