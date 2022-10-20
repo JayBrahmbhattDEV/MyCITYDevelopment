b1b984d94315c3d01c524a2d6ecabb3402f48ff7
Sagar <sagarbakraniya5@gmail.com>
2022-10-20 23:27:13 +0530

    MC-button - Add button component

--- my-city/src/app/shared/modules/shared/shared.module.ts
+++ my-city/src/app/shared/modules/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { McButtonComponent } from '../../components/mc-button/mc-button.component';
+import { McDashboardButtonComponent } from '../../components/mc-dashboard-button/mc-dashboard-button.component';
 import { McReportCardComponent } from '../../components/mc-report-card/mc-report-card.component';
 import { IonicModule } from '@ionic/angular';
+import { McButtonComponent } from '../../components/mc-button/mc-button.component';
 
 @NgModule({
-  declarations: [McButtonComponent, McReportCardComponent],
+  declarations: [
+    McDashboardButtonComponent,
+    McReportCardComponent,
+    McButtonComponent,
+  ],
   imports: [CommonModule, IonicModule],
-  exports: [McButtonComponent, McReportCardComponent],
+  exports: [
+    McDashboardButtonComponent,
+    McReportCardComponent,
+    McButtonComponent,
+  ],
 })
 export class SharedModule {}
