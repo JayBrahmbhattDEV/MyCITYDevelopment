2d433b4077fbc09669a3b690a9357a9376e5d1b2
JayBrahmbhattDEV <jaybrahmbhatt094@gmail.com>
2022-11-02 23:08:09 +0530

    temp commit

--- my-city/src/app/shared/components/mc-report-card/mc-report-card.component.ts
+++ my-city/src/app/shared/components/mc-report-card/mc-report-card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { CommonService } from 'src/app/services/common.service';
+import { ReportsService } from 'src/app/services/reports.service';
+import { MESSAGES } from 'src/app/utils/constants';
 
 @Component({
   selector: 'app-mc-report-card',
@@ -6,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mc-report-card.component.scss'],
 })
 export class McReportCardComponent implements OnInit {
+  reports = [];
+  address:any;
+  constructor(private reportService: ReportsService,
+    private commonService: CommonService,
+    private navController: NavController) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.getReports();
+  }
 
-  ngOnInit() {}
+  getReports() {
+    this.commonService.presentLoading(MESSAGES.FETHCHING_REPORTS);
+    this.reportService.getReports().subscribe(
+      (response: any) => {
+        if (response.success) {
+          this.reports = response.data;
+          for(let i = 0; i < this.reports.length; i++){
+            this.address = response.data[i].address;
+            console.log(this.address);
+          }
+        } else {
+          this.commonService.presentToaster({
+            message: MESSAGES.SOMETHING_WENT_WRONG_PLEASE_TRY_AGAIN_LATER,
+          });
+        }
+        this.commonService.hideLoading();
+      },
+      (e) => {
+        this.commonService.presentToaster({message:"Something went wrong!", color:'danger'})
+        this.commonService.hideLoading();
+      }
+    );
+  }
+
+  viewReport(report: any) {
+    this.navController.navigateForward('/view-report', {
+      state: report,
+    });
+  }
 
 }
